9a465b7274354e474f975bd7a14e4bc958346d47
Aslan <asl.zurabov@gmail.com>
2021-03-02 23:02:28 +0300

    3/2/2021 11:02PM

--- users/urls.py
+++ users/urls.py
@@ -1,13 +1,22 @@
 from django.urls import path
-from .views import UserRegisterView, ProfileUpdateView, Password_Manualy_ChangeView, password_success, ProfileShow
+from .views import (UserRegisterView,
+                    ProfileUpdateView,
+                    Password_Manualy_ChangeView,
+                    password_success,
+                    ProfileShow,
+                    loginview,
+                    logoutview,
+                    )
 from django.contrib.auth import views as auth_view
 
 urlpatterns = [
     path('register/', UserRegisterView.as_view(), name='register'),
+    path('login/',loginview,name='login'),
+    path('logout/',logoutview, name='logout'),
     path('<int:pk>/profile_show/', ProfileShow.as_view(), name='profile_show'),
     path('profile_update/', ProfileUpdateView.as_view(), name='profile_update'),
-    # path('password/', auth_view.PasswordChangeView.as_view(template_name="registration/change_password.html"),name='change_password'),
-    path('password/', Password_Manualy_ChangeView.as_view(template_name="registration/change_password.html"),
+    # path('password/', auth_view.PasswordChangeView.as_view(template_name="users/change_password.html"),name='change_password'),
+    path('password/', Password_Manualy_ChangeView.as_view(template_name="users/change_password.html"),
          name='change_password'),
     path('password_success/', password_success, name="password_success")
 ]
--- users/views.py
+++ users/views.py
@@ -1,28 +1,49 @@
-from django.shortcuts import render
-from django.views import generic
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
-from django.urls import reverse_lazy
-from .forms import RegistrationForm, ProfileChangeForms, UserProfileChangeForm, PasswordChangeManuallyForm
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.models import User
 from django.contrib.auth.views import PasswordChangeView
-from .models import Profile
 from django.shortcuts import get_object_or_404
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.views import generic
+
+from .forms import RegistrationForm, UserProfileChangeForm, PasswordChangeManuallyForm
+from .models import Profile
 
 
 class UserRegisterView(generic.CreateView):
     form_class = RegistrationForm
-    template_name = 'registration/registration.html'
+    template_name = 'users/registration.html'
     success_url = reverse_lazy('login')
 
 
 # def ProfileView(request):
 #
-#     return render(request, 'registration/profile_update.html')
+#     return render(request, 'users/profile_update.html')
+
+
+def loginview(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('myblog:home')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'users/login.html', {'form':form})
+
+
+def logoutview(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('myblog:home')
+    return render(request,'users/logout.html')
 
 
 class ProfileShow(generic.DetailView):
     model = Profile
-    template_name = "registration/profile_show.html"
+    template_name = "users/profile_show.html"
 
     def get_context_data(self, *args, **kwargs):
         context = super(ProfileShow, self).get_context_data(*args, **kwargs)
@@ -33,13 +54,9 @@ class ProfileShow(generic.DetailView):
         return context
 
 
-
-
-
-
 class ProfileUpdateView(generic.UpdateView):
     form_class = UserProfileChangeForm
-    template_name = 'registration/profile_update.html'
+    template_name = 'users/profile_update.html'
     success_url = reverse_lazy('myblog:home')
 
     def get_object(self):
@@ -52,4 +69,4 @@ class Password_Manualy_ChangeView(PasswordChangeView):
 
 
 def password_success(request):
-    return render(request, 'registration/password_success.html')
+    return render(request, 'users/password_success.html')
